5cace1148dda4c795c8a912b6d35645e8dcb6dbd
Amadeo <amadeocantarell@gmail.com>
2025-11-18 02:09:07 -0300

    asdasd

--- tests/loggin.spec.ts
+++ tests/loggin.spec.ts
@@ -23,3 +23,16 @@ test('Utilizando la extencion de PW y usando la opcion "record new"', async ({ p
   await page.getByRole('menuitem', { name: 'Logout' }).click();
   await expect(page).toHaveURL('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
 });
+
+test('Utilizando la extencion de PW y usando la opcion "record new" con otro nombre', async ({ page }) => {
+  await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+  await page.getByRole('textbox', { name: 'Username' }).click();
+  await page.getByRole('textbox', { name: 'Username' }).fill('Admin');
+  await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).fill('admin123');
+  await page.getByRole('button', { name: 'Login' }).click();
+  await expect(page).toHaveURL('https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index');
+  await page.getByText('rita yoe').click();
+  await page.getByRole('menuitem', { name: 'Logout' }).click();
+  await expect(page).toHaveURL('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+});

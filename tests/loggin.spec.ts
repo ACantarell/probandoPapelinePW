92cf5048dbc07db1ca361f471c7abbe7fd422430
Amadeo <amadeocantarell@gmail.com>
2025-11-19 23:31:19 -0300

    add test loggin

--- tests/loggin.spec.ts
+++ tests/loggin.spec.ts
@@ -9,7 +9,7 @@ test('Utilizando la herramienta npx playwright codegen ', async ({ page }) => {
   await page.getByRole('textbox', { name: 'Password' }).fill('admin123');
   await page.getByRole('button', { name: 'Login' }).click();
   await page.getByRole('link', { name: 'client brand banner' }).click();
-});
+})
 
 test('Utilizando la extencion de PW y usando la opcion "record new"', async ({ page }) => {
   await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
@@ -22,9 +22,9 @@ test('Utilizando la extencion de PW y usando la opcion "record new"', async ({ p
   await page.getByText('ABHINAND A').click();
   await page.getByRole('menuitem', { name: 'Logout' }).click();
   await expect(page).toHaveURL('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-});
+})
 
-test('Utilizando la extencion de PW y usando la opcion "record new" con otro nombre', async ({ page }) => {
+test('Utilizando la extencion de PW y usando la opcion "record new" 2', async ({ page }) => {
   await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
   await page.getByRole('textbox', { name: 'Username' }).click();
   await page.getByRole('textbox', { name: 'Username' }).fill('Admin');
@@ -32,7 +32,21 @@ test('Utilizando la extencion de PW y usando la opcion "record new" con otro nom
   await page.getByRole('textbox', { name: 'Password' }).fill('admin123');
   await page.getByRole('button', { name: 'Login' }).click();
   await expect(page).toHaveURL('https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index');
-  await page.getByText('rita yoe').click();
+  await page.getByText('ABHINAND A').click();
   await page.getByRole('menuitem', { name: 'Logout' }).click();
   await expect(page).toHaveURL('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-});
+})
+
+test('Utilizando la extencion de PW y usando la opcion "record new" 3', async ({ page }) => {
+  await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+  await page.getByRole('textbox', { name: 'Username' }).click();
+  await page.getByRole('textbox', { name: 'Username' }).fill('Admin');
+  await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).fill('admin123');
+  await page.getByRole('button', { name: 'Login' }).click();
+  await expect(page).toHaveURL('https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index');
+  await page.getByText('ABHINAND A').click();
+  await page.getByRole('menuitem', { name: 'Logout' }).click();
+  await expect(page).toHaveURL('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+})
+

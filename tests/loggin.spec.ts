dbec874a2bc2615ddc7fefe0d7633a0e09d7c240
Amadeo <amadeocantarell@gmail.com>
2025-11-19 23:26:11 -0300

    fix bug button

--- tests/loggin.spec.ts
+++ tests/loggin.spec.ts
@@ -19,7 +19,7 @@ test('Utilizando la extencion de PW y usando la opcion "record new"', async ({ p
   await page.getByRole('textbox', { name: 'Password' }).fill('admin123');
   await page.getByRole('button', { name: 'Login' }).click();
   await expect(page).toHaveURL('https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index');
-  await page.getByText('rita yoe').click();
+  await page.getByText('ABHINAND A').click();
   await page.getByRole('menuitem', { name: 'Logout' }).click();
   await expect(page).toHaveURL('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
 });
